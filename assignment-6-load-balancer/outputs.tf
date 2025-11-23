0c38d7227313d70d498c516ed3326db61281185f
Hongqian Li <hongqian.li@student.hamk.fi>
2025-11-23 20:55:28 +0100

    Add outputs for load balancer ID and backend pool ID in outputs.tf

--- assignment-6-load-balancer/outputs.tf
+++ assignment-6-load-balancer/outputs.tf
@@ -6,3 +6,13 @@ output "public_ip_address" {
   description = "The public IP address for accessing the load balancer"
   value       = azurerm_public_ip.main.ip_address
 }
+
+output "load_balancer_id" {
+  description = "ID of the load balancer"
+  value       = azurerm_lb.main.id
+}
+
+output "backend_pool_id" {
+  description = "ID of the backend address pool"
+  value       = azurerm_lb_backend_address_pool.main.id
+}
