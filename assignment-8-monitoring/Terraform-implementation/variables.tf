74b4d4a91bf88eda2e394ce0165df6910f43e6f3
Hongqian Li <hongqian.li@student.hamk.fi>
2026-01-07 19:33:28 +0100

    Add Terraform configuration for Assignment 8: Monitoring & Boot Diagnostics
    - Created .gitignore file to exclude sensitive and temporary files.
    - Added README.md for Assignment 8 with setup instructions and project details.
    - Implemented main.tf for Azure resources including VNet, VMs, Load Balancer, NSG, Storage, Backup, and Boot Diagnostics.
    - Created variables.tf for configurable parameters.
    - Developed outputs.tf to display deployment outputs.
    - Added install-nginx.sh script for automatic Nginx installation and custom index page creation.

---
+++ assignment-8-monitoring/Terraform-implementation/variables.tf
@@ -0,0 +1,57 @@
+# ============================================
+# Variables for Assignment 6
+# ============================================
+
+variable "resource_group_name" {
+  description = "Name of the resource group"
+  type        = string
+  default     = "rg-loadbalancer-assignment6"
+}
+
+variable "location" {
+  description = "Azure region"
+  type        = string
+  default     = "norwayeast"
+}
+
+variable "prefix" {
+  description = "Prefix for resource names"
+  type        = string
+  default     = "lb-a6"
+}
+
+variable "vnet_address_space" {
+  description = "Address space for virtual network"
+  type        = list(string)
+  default     = ["10.0.0.0/16"]
+}
+
+variable "subnet_address_prefix" {
+  description = "Address prefix for subnet"
+  type        = list(string)
+  default     = ["10.0.2.0/24"]
+}
+
+variable "vm_count" {
+  description = "Number of VMs"
+  type        = number
+  default     = 2
+}
+
+variable "vm_size" {
+  description = "Size of VMs"
+  type        = string
+  default     = "Standard_B1s" # vCPUs: 1, RAM: 1 GB, Temp storage: 4 GB and cheaper than "Standard_F2"
+}
+
+variable "admin_username" {
+  description = "Admin username for VMs"
+  type        = string
+  default     = "azureuser"
+}
+
+variable "ssh_public_key_path" {
+  description = "Path to SSH public key"
+  type        = string
+  default     = null
+}
